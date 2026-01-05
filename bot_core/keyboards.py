02de83b07d09c57f886a3c852ebb0b834c3b3b46
minupower-ship-it <minupower@gmail.com>
2026-01-05 13:30:19 -0700

    Update keyboards.py

--- bot_core/keyboards.py
+++ bot_core/keyboards.py
@@ -1,1 +1,16 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+def language_keyboard():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton("🇬🇧 English", callback_data='lang_en')],
+        [InlineKeyboardButton("🇸🇦 العربية", callback_data='lang_ar')],
+        [InlineKeyboardButton("🇪🇸 Español", callback_data='lang_es')]
+    ])
+
+def main_menu_keyboard(texts):
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton(texts["plans_btn"], callback_data='plans')],
+        [InlineKeyboardButton(texts["status_btn"], callback_data='status')],
+        [InlineKeyboardButton(texts["help_btn"], callback_data='help')]
+    ])
 
