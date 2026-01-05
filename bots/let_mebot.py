8414b9d5436dd5a083eecaa6f2518607405f3d14
minupower-ship-it <minupower@gmail.com>
2026-01-05 16:49:53 -0700

    Update let_mebot.py

--- bots/let_mebot.py
+++ bots/let_mebot.py
@@ -1,3 +1,5 @@
+import os
+
 # bots/let_mebot.py
 import asyncio
 import datetime
@@ -13,6 +15,9 @@ from config import *
 from bot_core.db import init_db, add_member, log_action, get_member_status
 from bot_core.keyboards import main_menu_keyboard, plans_keyboard, payment_keyboard
 from bot_core.texts import get_text
+
+PORTAL_RETURN_URL = os.environ.get("LETMEBOT_PORTAL_RETURN_URL")
+
 
 stripe.api_key = STRIPE_SECRET_KEY
 
