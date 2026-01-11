938c11187c1f8484adb77e0ce0e2484553469e57
minupower-ship-it <minupower@gmail.com>
2026-01-10 17:14:17 -0700

    Update morevids_bot.py

--- bots/morevids_bot.py
+++ bots/morevids_bot.py
@@ -1,18 +1,22 @@
 # bots/morevids_bot.py
 from bot_core.base_bot import BaseBot
-from config import MOREVIDS_TOKEN, MOREVIDS_PRICE_MONTHLY, MOREVIDS_PRICE_LIFETIME, MOREVIDS_PORTAL_RETURN_URL, PAYPAL_MOREVIDS_MONTHLY, PAYPAL_MOREVIDS_LIFETIME, WELCOME_VIDEO_MOREVIDS
+from config import MOREVIDS_TOKEN, MOREVIDS_PRICE_MONTHLY, MOREVIDS_PRICE_LIFETIME, MOREVIDS_PRICE_WEEKLY, MOREVIDS_PORTAL_RETURN_URL, PAYPAL_MOREVIDS_MONTHLY, PAYPAL_MOREVIDS_LIFETIME, PAYPAL_MOREVIDS_WEEKLY, WELCOME_VIDEO_MOREVIDS
 
 class MoreVidsBot(BaseBot):
-    def __init__(self):
-        super().__init__(
+    def init(self):
+        super().init(
             bot_name='morevids',
             token=MOREVIDS_TOKEN,
+            price_weekly=MOREVIDS_PRICE_WEEKLY,
             price_monthly=MOREVIDS_PRICE_MONTHLY,
             price_lifetime=MOREVIDS_PRICE_LIFETIME,
             welcome_video=WELCOME_VIDEO_MOREVIDS,
+            paypal_weekly=PAYPAL_MOREVIDS_WEEKLY,
             paypal_monthly=PAYPAL_MOREVIDS_MONTHLY,
             paypal_lifetime=PAYPAL_MOREVIDS_LIFETIME,
+            has_weekly=True,
             has_monthly=True,
             has_lifetime=True,
             portal_return_url=MOREVIDS_PORTAL_RETURN_URL
         )
+
